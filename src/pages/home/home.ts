66907801f171a591910199710c4bf540f61ad9b4
BEN CHEIKH <ramy.bencheikh@ynov.com>
2017-10-31 14:24:28 +0100

    auto deploy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  app: any = { nom: String, version: Number};
 
   constructor(public navCtrl: NavController) {
-
+    this.app.nom = "Nom App";
+    this.app.version = 0.1;
   }
 
+}
+class App {
+constructor(public nom: String, public version: Number){
+
+ }
 }
